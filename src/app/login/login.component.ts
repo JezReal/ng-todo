9516fc24d7d19678302ac16676d632bfa06f1062
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-10 11:43:38 +0800

    create login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup 
+  
+  constructor(
+    private fb: FormBuilder,
+    private dataService: DataService,
+    private router: Router
+  ) { 
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(event: any) {
+    // e.preventDefault();
+
+    if (this.loginForm.invalid) {
+      console.log('Invalid Input');
+    } else {
+      // console.log(this.loginForm.value);
+      this.dataService
+        .login(this.loginForm.value)
+        .subscribe((res: any) => {
+          // console.log(res.data);
+          if (res.data) {
+            console.log('successfully logged in');
+            window.sessionStorage.setItem(
+              'account_id',
+              res.data.account_id
+            );
+            this.router.navigate(['home']);
+          } else if (res.error) {
+            console.log('invalid email or password');
+            
+          }
+        });
+    }
+  }
 }
