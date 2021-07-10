90cc2b73128bb21570b1db57a31b1974d12504db
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-10 11:10:46 +0800

    setup service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private link = 'http://localhost/todo/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { 
+    
+  }
+
+  login(data: any) {
+    return this.httpClient.post(this.link + "login", JSON.stringify(data));
+  }
+
+  register(data: any) {
+    return this.httpClient.post(this.link + "register", JSON.stringify(data));
+  }
 }
