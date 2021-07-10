fa008da5918fbece744f4be143585d88a5aca2fe
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-10 16:48:43 +0800

    implement register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup 
+  
+  constructor(
+    private fb: FormBuilder,
+    private dataService: DataService,
+    private router: Router
+  ) { 
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(event: any) {
+    // e.preventDefault();
+
+    if (this.registerForm.invalid) {
+      alert('Invalid input')
+    } else {
+      // console.log(this.loginForm.value);
+      this.dataService
+        .register(this.registerForm.value)
+        .subscribe((res: any) => {
+          if (res.data) {
+            alert('Successfully registered. Please login')
+            this.router.navigate(['/']);
+          } else if (res.error) {
+            alert('Registration failed')
+          }
+        });
+    }
+  }
 }
